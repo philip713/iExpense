ce9e7e1c60329cc8bf90a4ef133ddd5f63b80cec
philip713 <105677806+philip713@users.noreply.github.com>
2025-08-08 09:07:49 -0600

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Philip Janzel Paradeza on 2025-08-08.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
